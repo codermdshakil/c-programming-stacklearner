1dc27397cf5c2aa619dcfd7852b057671944a0dd
codermdshakil <ahmedshakil0512@gmail.com>
2024-03-15 11:28:33 +0600

    learned C program basic calculation +, -, *, /

---
+++ 01.intro/print.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Shakil Ahmed Is \t now Programmers\n");
+    printf("C is interesting language\n");
+    printf("I am a Teacher\n");
+    printf("I very Excaited to learning C programming\n");
+    printf("I very Excaited to learning C programming lorem, \n hello vai valoi lagtase\n");
+
+    printf("%d\n", 5 + 20);
+    printf("%d\n", 20 - 5);
+    printf("%d\n", 5 * 20);
+    printf("%d\n", 20 / 5);
+
+    return 0;
+}
+
+/**
+ * \n new line
+ * \t for tab
+ * %d means  integer
+ *
+ */
