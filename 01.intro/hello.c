865b6547d9ffb145128d82b94aa40a428729139f
codermdshakil <ahmedshakil0512@gmail.com>
2024-03-15 11:01:16 +0600

    Now I can run C program

---
+++ 01.intro/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world. Yah mamma Now I can Run C program In my laptop\n");
+    return 0;
+}
+
+/**
+ * How code run
+ * step 1 - Editing
+ * step 2 - Compilint
+ * step 3 - linking
+ * step 4 - executing
+ *
+ * include means import
+ * <stdio.h> - stdio means standard input output h means header
+ * printf - show output
+ * \n remove %
+ * must use douoble quatation
+ *
+ */
