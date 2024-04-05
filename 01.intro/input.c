a69310ae836876b5922c6396294e15a72bd4f37d
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-05 21:29:38 +0600

    how to take input from user

---
+++ 01.intro/input.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+    float c, d;
+    char s;
+
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &c, &d);
+    scanf("%c", &s);
+
+    // printf("%d\n", a);
+    // printf("%d\n", b);
+    // printf("%0.2f\n%0.2f\n", c, d);
+
+    printf("%c", s);
+    return 0;
+}
+
+/**
+ * 009. User Input in C
+ *-----------------------
+
+ * scanf - Using  we can take input from users
+ */
