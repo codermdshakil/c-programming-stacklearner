fb1d4b4d7c1eea33847d84885d25926ad62e035f
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-05 21:16:12 +0600

    video 8 const and reserved keyword

---
+++ 01.intro/constant-reserved-keyword.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    const float PI = 3.1416f; // using const we can make constant cariable
+    printf("%f \n", PI);
+    return 0;
+}
+
+// All Capital letter use for constant variable name
+// Constant value need some times then we are use const
+// - using const we can make constant cariable
+// we can't use reserved keyword as a variable name
