1b462e74cf8dcc4b3f289964ebeb145e84b19de7
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-19 15:03:39 +0600

    Learn : Octal, hexadecimal and decimals of octal and hexadeciaml

--- 2-Integer Data types/integer-data.c
+++ 2-Integer Data types/integer-data.c
@@ -4,23 +4,31 @@ int main()
 {
     // Signed Integer data
 
-    // short - %d, %hi 
+    // short - %d, %hi
     short a = 45;
     printf("I am short - %hi\n", a);
 
-    // int - %d, %i 
+    // int - %d, %i
     int b = 1000;
     printf("I am int - %d\n", b);
 
-
-    // long - %ld, %li 
+    // long - %ld, %li
     long c = 1200L;
     printf("I am long - %ld\n", c);
 
-    // long Long - %lld, %lli 
+    // long Long - %lld, %lli
     long long d = 1234500LL;
     printf("I am long long - %lld\n", d);
 
+    // Octal int - %o
+    int o = 0777;
+    printf("I am Octal - %o\n", o);
+    printf("I am decimal of Octal - %d\n", o);
+
+    // Octal int - %x, %X
+    int h = 0Xfff;
+    printf("I am Hexadecimal - %x\n", h);
+    printf("I am deciaml of Hexadecimal - %d\n", h);
 
     return 0;
 }
