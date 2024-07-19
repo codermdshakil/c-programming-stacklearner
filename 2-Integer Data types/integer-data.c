83ca6fe0f278a8766e911f56c268fa57ee7c25eb
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-19 14:57:46 +0600

    Learn : long long

--- 2-Integer Data types/integer-data.c
+++ 2-Integer Data types/integer-data.c
@@ -6,16 +6,20 @@ int main()
 
     // short - %d, %hi 
     short a = 45;
-    printf("I am short %hi\n", a);
+    printf("I am short - %hi\n", a);
 
     // int - %d, %i 
     int b = 1000;
-    printf("I am int %d\n", b);
+    printf("I am int - %d\n", b);
 
 
-    // long - %dl, %il 
+    // long - %ld, %li 
     long c = 1200L;
-    printf("I am long %dl\n", c);
+    printf("I am long - %ld\n", c);
+
+    // long Long - %lld, %lli 
+    long long d = 1234500LL;
+    printf("I am long long - %lld\n", d);
 
 
     return 0;
