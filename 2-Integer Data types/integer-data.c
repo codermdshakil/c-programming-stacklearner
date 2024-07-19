9175b3540f5d320e12f92f96ec9080abe51b7d1d
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-19 15:12:02 +0600

    Learn : unsigned short, int, long, long long

--- 2-Integer Data types/integer-data.c
+++ 2-Integer Data types/integer-data.c
@@ -3,6 +3,8 @@
 int main()
 {
     // Signed Integer data
+    printf("Signed Integer\n");
+    printf("\n");
 
     // short - %d, %hi
     short a = 45;
@@ -29,6 +31,28 @@ int main()
     int h = 0Xfff;
     printf("I am Hexadecimal - %x\n", h);
     printf("I am deciaml of Hexadecimal - %d\n", h);
+
+    printf("\n");
+    // Unsigned Integer
+    printf("Unsigned Integer\n");
+    printf("\n");
+
+    // unsigned short - %hu
+    unsigned short us = 41;
+    printf("Unsigned short - %hu\n", us);
+
+    // unsigned int - %u
+    unsigned int ui = 4321;
+    printf("Unsigned int - %u\n", ui);
+
+
+    // unsigned long - %lu
+    unsigned long ul = 4325314;
+    printf("Unsigned long - %lu\n", ul);
+
+    // unsigned long Long - %llu
+    unsigned long long ull = 4325314452462354345;
+    printf("Unsigned long long - %llu\n", ull);
 
     return 0;
 }
