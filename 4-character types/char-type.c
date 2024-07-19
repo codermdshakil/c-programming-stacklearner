5ec04578bf8ba18f8de952cb91e5a8c10c6054dd
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-19 23:40:02 +0600

    Learn : char and ASCII Codes

---
+++ 4-character types/char-type.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = 'A';
+    // char a1 = 65;
+
+    // char b = 'B' + 3;
+    // printf("Char - %c\n", a);
+    // printf("Char - %c\n", a1);
+    // printf("Char - %c\n", b);
+    
+    return 0;
+}
+
+/**
+ * Summary
+ * ASCII Codes
+ * A - Z => 65 - 90
+ * a - z => 97 - 122
+ * a - z => 48 - 57
+*/
