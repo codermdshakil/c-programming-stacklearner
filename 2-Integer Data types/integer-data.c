cf6b35be62c9e75a4f9253ee04b96e5e7d49ed81
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-19 14:53:55 +0600

    Learn : short, int, long

---
+++ 2-Integer Data types/integer-data.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // Signed Integer data
+
+    // short - %d, %hi 
+    short a = 45;
+    printf("I am short %hi\n", a);
+
+    // int - %d, %i 
+    int b = 1000;
+    printf("I am int %d\n", b);
+
+
+    // long - %dl, %il 
+    long c = 1200L;
+    printf("I am long %dl\n", c);
+
+
+    return 0;
+}
