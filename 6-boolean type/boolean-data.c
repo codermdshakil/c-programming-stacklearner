a24a39953363fc137fe31f7fc7f5cb4791452c6f
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-20 19:41:48 +0600

    Learn : bool in c

---
+++ 6-boolean type/boolean-data.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    // short user_logged_in = 0;
+    bool user_logged_in = true;
+    if (user_logged_in)
+    {
+        printf("User Logged In\n");
+    }
+    else
+    {
+
+        printf("User Logged Out\n");
+    }
+    return 0;
+}
+/**
+ * bool - header file -> <stdbool.h>
+ * 
+*/
