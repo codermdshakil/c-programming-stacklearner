747d27272c097ad520f99752d6215bc41aefa890
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-20 21:54:01 +0600

    Learn : Types of Operators -> Unary, Binary, Ternary

---
+++ 3-Operators/1_intro/2_types_operators.c
@@ -0,0 +1,21 @@
+/**
+ * Mainly three types of operators
+ * 1. Unary
+ * 2. Binary
+ * 3. Ternary
+ * 
+ * 1. Unary Operators: Unary operators have one operand.
+ * 
+ * What is oparand?
+ * => a + b here + is Operator and a, b operand.
+ * 
+ * 2. Binary Operators : Binary operators have two operand. There are different types of binary operators
+ * 
+ * - Arithmetic (+, -, * , /, %)
+ * - Relational (>,<, >=, <= , ==, !=)
+ * - Logical (&&, ||, !)
+ * - Assignment (+=, -=, *=, /=, %=)
+ * - Bitwise (&, |, ^, ~, <<, >>)
+ * 
+ * 3. Ternary Operators : Ternary Operators have three operand. Ex- Conditional Operators
+*/
