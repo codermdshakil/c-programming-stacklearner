16742c924908ba9fa1d53d281d71f8b049a152b6
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-20 22:29:52 +0600

    Learn : Arithmetic Operators

---
+++ 3-Operators/2-Arithmetic/arithmetic_operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 30;
+    int b = 7;
+
+    printf("Addition : %d\n", a + b);
+    printf("Substraction : %d\n", a - b);
+    printf("Multipication : %d\n", a * b);
+    printf("Division : %d\n", a / b);
+    printf("Remainder : %d\n", a % b); // % return ভাগশেষ
+
+    return 0;
+}
+
+/**
+ * Atihmetic operators : +,-,*,/,%
+ */
