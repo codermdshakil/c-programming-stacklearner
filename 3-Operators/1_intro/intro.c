42f5f1eeb28f4f33b826e4a278508f6c37e314f7
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-20 20:21:52 +0600

    Learn : What is opetator and what does it work?

---
+++ 3-Operators/1_intro/intro.c
@@ -0,0 +1,10 @@
+//1. What is Operators?
+// => Operators are some specials symbol who performs some special operation and meaningful operations. Like - addition, multipication,substraction, division.
+
+//2. What does operators do?
+// -> Basically operator do some meaningfull operations
+// -> Operators act a very important rule in Programming
+// -> Because Programming all about Mathmatics and Operations
+
+// so we need operator to perform those operations
+
