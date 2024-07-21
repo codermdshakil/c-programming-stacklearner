2057caa55dd7cb8e2f1304cd5cf6b71a4450fb1d
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-21 11:32:19 +0600

    Learn : Condition

---
+++ 4-conditions/0_conditions.c
@@ -0,0 +1,15 @@
+/**
+ * Condition
+ * 
+ * What is Condition?
+ * Conditions are way to implement a logical situation in programming. Sometimes we have different situations and based on one situation we have to execute some sort of code rather then other situation.
+ * 
+ * Example :
+ * If a user logged in then show this name, otherwise say please log in
+ * 
+ * Conditional Statement in C progamming
+ * 1.If condition
+ * 2.Else condition
+ * 3.Else If condition
+ * 4.Switch statement
+*/
