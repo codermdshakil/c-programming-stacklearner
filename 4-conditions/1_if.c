8c185e281a01e0bd2191c394fa36366bec199164
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-21 19:28:55 +0600

    Learn : if condition in c

---
+++ 4-conditions/1_if.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    // if structure
+    //--------------
+    // if (true)
+    // {
+    // I will execute
+    // }
+
+    // int condition = 1;
+    // if (condition)
+    // {
+    //     printf("I will get a bat\n");
+    // }
+
+    int a = 23;
+    int b = 15;
+
+    if (a > b)
+    {
+        printf("You are Elder\n");
+    }
+    return 0;
+}
