73c5428192b6146a7c4ad74930f65abac077a1c2
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-21 21:54:58 +0600

    Learn Tips : switch case

--- 4-conditions/4_switch_case.c
+++ 4-conditions/4_switch_case.c
@@ -5,28 +5,45 @@ int main()
     int day_number;
     scanf("%d", &day_number);
 
+    // switch (day_number)
+    // {
+    // case 0:
+    //     printf("Saturday\n");
+    //     break;
+    // case 1:
+    //     printf("Sunday\n");
+    //     break;
+    // case 2:
+    //     printf("Monday\n");
+    //     break;
+    // case 3:
+    //     printf("Tuesday\n");
+    //     break;
+    // case 4:
+    //     printf("Wenesday\n");
+    //     break;
+    // case 5:
+    //     printf("Thursday\n");
+    //     break;
+    // case 6:
+    //     printf("Friday\n");
+    //     break;
+    // default:
+    //     printf("Wrong day Number\n");
+    // }
+
     switch (day_number)
     {
     case 0:
-        printf("Saturday\n");
+    case 6:
+        printf("OFF day\n");
         break;
     case 1:
-        printf("Sunday\n");
-        break;
     case 2:
-        printf("Monday\n");
-        break;
     case 3:
-        printf("Tuesday\n");
-        break;
     case 4:
-        printf("Wenesday\n");
-        break;
     case 5:
-        printf("Thursday\n");
-        break;
-    case 6:
-        printf("Friday\n");
+        printf("Work day\n");
         break;
     default:
         printf("Wrong day Number\n");
