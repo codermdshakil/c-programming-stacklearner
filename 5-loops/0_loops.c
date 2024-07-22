510597719d8dc3b0573da779553c4737e865bdbf
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-22 16:33:02 +0600

    Learn :what is loop and types of loops

---
+++ 5-loops/0_loops.c
@@ -0,0 +1,19 @@
+/**
+ * What is Loop?
+ * - Loop is a statement which repeat a given task a given amount of time.
+ * 
+ * Example- Printing name on the screen, counting 1 - 100, find even number between 1- 100.
+ * 
+ * Why do we use Loops?
+ * 
+ * => In programming we always  some repetitive task. to reduce code we use loop.
+ * 
+ * Example: We have a student database and we want a sent a notice. If we want it to be done manually it will become nearly impossible.
+ * 
+ * Types of loops in c:
+ * 
+ * - For loop (Follow steps)
+ * - While Loop (Follow Condition)
+ * - Do While Loop (Follow Condition but একবার হলে ও চলবে)
+ * 
+*/
