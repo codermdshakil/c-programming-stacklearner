fed154850d6db3fb37d57916d81a17a54774a75b
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-23 00:22:19 +0600

    Learn : Pyramid using loop

--- 5-loops/9_pyramid.c
+++ 5-loops/9_pyramid.c
@@ -3,6 +3,33 @@
 int main()
 {
 
+    // Half pyramid - by HM nayem
+    int rows = 10;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Full Pyramid
+    for (int i = 0; i < rows; i++)
+    {
+        // print space
+        for (int j = i + 1; j < rows; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < (2 * i) - 1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     /*These code is my code */
     // half pyramid
     // int n1 = 5;
@@ -22,7 +49,7 @@ int main()
     // int n = 5;
     // int star = 1;
     // int space = n - 1;
-    
+
     // printf("\n Full Pyramid\n \n");
     // for (int i = 0; i < n; i++)
     // {
