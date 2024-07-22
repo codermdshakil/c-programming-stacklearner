3c89832aab77ded79d25f2f469a9b04dd769c789
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-23 00:11:24 +0600

    Practise : half and full pyramid using loop

---
+++ 5-loops/9_pyramid.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /*These code is my code */
+    // half pyramid
+    // int n1 = 5;
+    // int start1 = 1;
+    // printf("\n Half pyramid\n \n");
+    // for (int i = 0; i < n1; i++)
+    // {
+    //     for (int j = 1; j <= start1; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    //     start1++;
+    // }
+
+    // full pyramid
+    // int n = 5;
+    // int star = 1;
+    // int space = n - 1;
+    
+    // printf("\n Full Pyramid\n \n");
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // print space
+    //     for (int j = 1; j <= space; j++)
+    //     {
+    //         printf(" ");
+    //     }
+
+    //     for (int k = 1; k <= star; k++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    //     star += 2;
+    //     space--;
+    // }
+
+    return 0;
+}
