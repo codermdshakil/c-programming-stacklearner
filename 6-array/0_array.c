e0b186653c04c6da948d26749346252f7bbb2a78
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-23 18:07:38 +0600

    Learn : array

--- 6-array/0_array.c
+++ 6-array/0_array.c
@@ -1,3 +1,3 @@
 /**
- * Array is a collection of same of data typed data that stored like key and value. We can access value using index
+ * Array is a collection of same of data typed data that stored like key and value. Array collect multiple data. We can access value using index
 */
