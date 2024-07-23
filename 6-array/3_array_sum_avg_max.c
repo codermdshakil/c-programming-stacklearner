396c8571ab06ab7942f0c82c00cf1877d89b68f5
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-23 21:00:10 +0600

    Learn : max, average, min

--- 6-array/3_array_sum_avg_max.c
+++ 6-array/3_array_sum_avg_max.c
@@ -11,37 +11,39 @@ int main()
     scores[4] = 83;
 
     // find sum of scores
-    int scores_sum = 0;
-    for (int i = 0; i < 5; i++)
-    {
-        scores_sum += scores[i];
-    }
-    printf("Scores Sum - %d\n", scores_sum);
+    // int scores_sum = 0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     scores_sum += scores[i];
+    // }
+    // printf("Scores Sum - %d\n", scores_sum);
 
     // find average scores
 
-    printf("Average Score - %d\n", scores_sum / 5);
+    // printf("Average Score - %d\n", scores_sum / 5);
 
     // find highest scores
     int mx = INT_MIN;
     for (int i = 0; i < 5; i++)
     {
+        // find max
         if (mx < scores[i])
         {
             mx = scores[i];
         }
     }
+
     printf("Max - %d\n", mx);
     // find min scores
-    int mn = INT_MAX;
-    for (int i = 0; i < 5; i++)
-    {
-        if (mn > scores[i])
-        {
-            mn = scores[i];
-        }
-    }
-    printf("Min - %d\n", mn);
+    // int mn = INT_MAX;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (mn > scores[i])
+    //     {
+    //         mn = scores[i];
+    //     }
+    // }
+    // printf("Min - %d\n", mn);
 
     return 0;
 }
