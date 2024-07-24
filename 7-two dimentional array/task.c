9b1810cf3d57000616cbdbae5cb159912f8e2467
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-25 01:10:28 +0600

    Task : 10 tasks added

---
+++ 7-two dimentional array/task.c
@@ -0,0 +1,14 @@
+/**
+ * Assignments
+ * 1. Write a programs that reads in 2D array of integers and print out the all elements sum.
+ * 2. Write a programs that reads in 2D array of integers and print out the values of the elements in the array that are greater then 10.
+ * 3. Write a programs that reads in 2D array of integers and print out the values of the elements in the arrat that divisiable by 3.
+ * 4. Write a programs that reads in 2D array of integers and calculate the product of elements  in each row and cols
+ * 5. Write a programs that reads in 2D array of integers and calculate their sum and differents.
+ * 6. Write a programs that reads in 2D array of integers and print out the transpose of the array
+ * 7. Write a programs that reads in 2D array of characters and print out the values of the elements in the array that are vowels
+ * 8. Write a programs that reads in 2D array of integers and check if it is a symmentic matrix
+ * 9. Write a programs that reads in 2D array of integers and check if it is a diagonal matrix
+ * 10. Write a programs that reads in 2D array of integers and check if it is a lower trignangular matrix
+ *
+*/
