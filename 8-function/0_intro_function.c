5d2d68d0c5cc1a44db64cea9c2c75137c88031a5
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-26 21:23:38 +0600

    Learn : what is function

---
+++ 8-function/0_intro_function.c
@@ -0,0 +1,6 @@
+/**
+ * What is a function?
+ * => Function is a piece of code that can do something for us when we call it.
+ * Ex. I have a bag of marbles and now i want to count how many marbles in the bag in case i create a simple function of counting and i call this function to calculate my bag marbles
+ * Ex. I want to make a pizza I dont know how to make it.  My mother create a function where write down step by step process how to make pizza. Now i want to make I should call this function can this function guide me hoe to make pizza .
+*/
