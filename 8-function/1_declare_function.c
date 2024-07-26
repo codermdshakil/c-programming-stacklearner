b3aa62f4c692aa5c72a0833c493232c4253c05b2
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-26 22:31:47 +0600

    Learn : scope , local and global variable

--- 8-function/1_declare_function.c
+++ 8-function/1_declare_function.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 // declare a function
+
+int age = 20; // global vairbale
+
 void my_first_function()
 {
-    int age = 20;
+    int age = 25; // local vairbale
     printf("Shakil Ahmed Age is %d\n", age);
 }
 
