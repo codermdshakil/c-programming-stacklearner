a50a16633a7441ca7051aa0b46a42f63db8396a1
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-26 23:49:57 +0600

    Learn : Importance of function

---
+++ 8-function/5_important_of_function.c
@@ -0,0 +1,13 @@
+/**
+ * Importance of function
+ * 
+ * Code Reuseability : functions allow you to write a block of code once it use multiple times in differents parts of your program.This can save your time and effort in writting and testing code, and can also your program easier to maintain.
+ * 
+ * Modularity : Funcitons allow you to break down large program into smaller more manageable pieces. Each function can perform a specific task or set of task, which makes your code mre organized and easier to understand.
+ * 
+ * Abstraction: Functions can hide the details implementation from the rest of the program. This means that others part of the program only need to know what the function does, now how it does it. this can make your code easier to read and understand.
+ * 
+ * Testing and Debugging : Function can be tested and debugged separately from the rest of the program. It can make easier to identify and fix error in your code.
+ * 
+ * 
+*/
