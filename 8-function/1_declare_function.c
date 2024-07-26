e399d0152b1fac50d14feb9d8f27ab9204bec613
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-26 22:21:11 +0600

    Learn : function declare and invoke

---
+++ 8-function/1_declare_function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// declare a function
+void my_first_function()
+{
+    int age = 20;
+    printf("Shakil Ahmed Age is %d\n", age);
+}
+
+int main()
+{
+    printf("Hello world\n");
+    my_first_function();
+
+    return 0;
+}
