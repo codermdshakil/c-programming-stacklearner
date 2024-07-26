331900e52b2a6617a1e2463b9615d3e104990a69
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-26 22:36:18 +0600

    Learn : why we need function

--- 8-function/1_declare_function.c
+++ 8-function/1_declare_function.c
@@ -10,10 +10,28 @@ void my_first_function()
     printf("Shakil Ahmed Age is %d\n", age);
 }
 
+void squr10()
+{
+    printf("%d\n", 10 * 10);
+}
+
 int main()
 {
     printf("Hello world\n");
     my_first_function();
+    my_first_function();
+    my_first_function();
+    my_first_function();
+    my_first_function();
+    my_first_function();
+    my_first_function();
+    squr10();
+    squr10();
+    squr10();
+    squr10();
+    squr10();
+    squr10();
+    squr10();
 
     return 0;
 }
