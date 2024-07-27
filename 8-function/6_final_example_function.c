6fa06666a0fffe545886dcef58ab93ebaa6815d3
codermdshakil <ahmedshakil0512@gmail.com>
2024-07-27 22:23:37 +0600

    Created : Create sum function

--- 8-function/6_final_example_function.c
+++ 8-function/6_final_example_function.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+// Prototype 
+int sum(int a, int b);
+int sub(int a, int b);
+void print(int a, int b, char sign);
+
+// create sum function to calculate 2 numbers sum
+int sum(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
 
 int main()
 {
     // We have to create a calculator that can calculate sub and sum. The print the result structured way.
-    
+
     return 0;
 }
